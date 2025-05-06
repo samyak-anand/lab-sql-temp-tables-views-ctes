2f6431090924fbf93e79a7450de4b451fcfd88b0
Samyak Anand <samyak.anand@gmail.com>
2025-05-06 15:13:06 +0200

    partial soft

---
+++ lab.sql
@@ -0,0 +1,51 @@
+/*
+
+Creating a Customer Summary Report
+
+In this exercise, you will create a customer summary report that summarizes key information about customers in the Sakila database, including their rental history and payment details. 
+The report will be generated using a combination of views, CTEs, and temporary tables.
+
+Step 1: Create a View
+First, create a view that summarizes rental information for each customer. 
+The view should include the customer's ID, name, email address, and total number of rentals (rental_count).
+
+Step 2: Create a Temporary Table
+Next, create a Temporary Table that calculates the total amount paid by each customer (total_paid). 
+The Temporary Table should use the rental summary view created in Step 1 to join with the payment table and calculate the total amount paid by each customer.
+
+Step 3: Create a CTE and the Customer Summary Report
+Create a CTE that joins the rental summary View with the customer payment summary Temporary Table created in Step 2. 
+The CTE should include the customer's name, email address, rental count, and total amount paid.
+
+Next, using the CTE, create the query to generate the final customer summary report, which should include:
+customer name, email, rental_count, total_paid and average_payment_per_rental, this last column is a derived column from total_paid and rental_count.
+
+*/
+
+
+#Creating view
+
+use sakila;
+
+CREATE  OR REPLACE  VIEW rental_summary  AS
+SELECT concat(first_name, ' ', last_name) AS 'customer_name',
+		c.customer_id,
+		c.email
+	FROM customer c 
+		inner join rental r on c.customer_id = r.customer_id
+	GROUP BY
+    c.customer_id, c.first_name, c.last_name, c.email;
+        
+
+
+CREATE TEMPORARY TABLE customer_payment2 AS
+SELECT
+    rsv.customer_id,
+    SUM(p.amount) AS total_paid
+FROM rental_summary rsv
+JOIN payment p ON rsv.customer_id = p.customer_id
+GROUP BY rsv.customer_id;
+
+
+
+select * from customer_payment2       
